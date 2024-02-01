1da778ac5350ad9cf72e46f48e3e1e499f43f412
Karl Tiama <karltiama@gmail.com>
2024-02-01 15:38:15 -0500

    feat: add basic form

---
+++ src/app/components/Form.tsx
@@ -0,0 +1,56 @@
+'use client'
+import React, { useState, FormEvent, ChangeEvent } from 'react'
+
+interface FormData {
+  points: string;
+  notes: string;
+}
+interface FormProps {
+  onSubmit: (data: FormData) => void;
+}
+
+const Form: React.FC<FormProps> = ({ onSubmit }) => {
+  const [data, setData] = useState<FormData>({points: '', notes: ''})
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setData({ ...data, [e.target.name] : e.target.value });
+  }
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(data);
+    // onSubmit(data);
+  }
+
+  return (
+    <div className='flex justify-center items-center '>
+      <form 
+        className='bg-slate-500 rounded shadow-lg p-4 m-4 flex flex-col space-y-4'
+        onSubmit={handleSubmit}
+      >
+        <input 
+          type='number'
+          name='points'
+          className='p-2 rounded text-black'
+          value={data.points}
+          onChange={handleChange}
+        />
+        <textarea
+          name="notes"
+          className='p-2 rounded text-black'
+          value={data.notes}
+          onChange={handleChange}
+        />
+        <button 
+          type='submit'
+          className='bg-blue-500 text-white p-2 rounded hover:bg-blue-600'
+        >
+          Add Game
+        </button>
+      </form>
+    </div>
+    
+  )
+}
+
+export default Form;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Header from "./components/Header";
 import LandingPage from "./components/LandingPage";
+import Form from "./components/Form";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
-      <Header />
-      <LandingPage />
+      <Form />
     </main>
   );
 }
