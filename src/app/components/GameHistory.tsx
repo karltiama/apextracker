906a7e00101374aba9c6dc72f6721fcf4a311121
Karl Tiama <karltiama@gmail.com>
2024-02-05 19:41:16 -0500

    refactor: made centering and visual changes

--- src/app/components/GameHistory.tsx
+++ src/app/components/GameHistory.tsx
@@ -12,21 +12,23 @@ interface GameHistoryProps {
 
 const GameHistory: React.FC<GameHistoryProps> = ({ games }) => {
   return (
-    <div className="mt-8">
-      <h2 className="text-xl font-bold mb-4">Game History</h2>
-      <ul className="list-none">
-        {games.length > 0 ? (
-          games.map((game) => (
-            <li key={game.id} className="mb-2 p-2 bg-gray-200 rounded">
-              <p className='p-2 rounded text-black'><strong>Points:</strong> {game.points}</p>
-              <p className='p-2 rounded text-black'><strong>Notes:</strong> {game.notes}</p>
-              {/* Display additional game data here */}
-            </li>
-          ))
-        ) : (
-          <p>No game history to display.</p>
-        )}
-      </ul>
+    <div className="mt-8 w-full flex justify-center">
+      <div className="max-w-2xl w-full">
+        <h2 className="text-xl font-bold mb-4 text-center">Game History</h2>
+        <ul className="list-none space-y-4">
+          {games.length > 0 ? (
+            games.map((game) => (
+              <li key={game.id} className="mb-2 p-4 bg-gray-200 rounded shadow">
+                <p className="rounded text-black"><strong>Points:</strong> {game.points}</p>
+                <p className="mt-2 rounded text-black"><strong>Notes:</strong> {game.notes}</p>
+                {/* Display additional game data here */}
+              </li>
+            ))
+          ) : (
+            <p className="text-center">No game history to display.</p>
+          )}
+        </ul>
+      </div>
     </div>
   );
 };
