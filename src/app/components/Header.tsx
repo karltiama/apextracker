dcff2d683ab7feae8316c89e5e681ab2201b09fd
Karl Tiama <karltiama@gmail.com>
2024-02-16 19:16:52 -0500

    feat: use Link tags instead of div tags

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,21 +6,30 @@ const Header = () => {
       <section className='layout'>
         <div className='flex justify-between'>
           <div className='flex space-x-4'>
-              <div>Home</div>
-              <div>Blog</div>
-              <div>Projects</div>
-              <div>Shorts</div>
+            {/* Using Link components for navigation */}
+            <Link href="/" className='hover:text-gray-300'>
+              Home
+            </Link>
+            <Link href="/blog" className='hover:text-gray-300'>
+              Blog
+            </Link>
+            <Link href="/projects" className='hover:text-gray-300'>
+              Projects
+            </Link>
+            <Link href="/shorts" className='hover:text-gray-300'>
+              Shorts
+            </Link>
           </div>
-            {/* Right Aligned Items */}
+          {/* Right Aligned Items */}
           <div>
             <div>Icon</div>
           </div>
         </div>
-          {/* Logo and Brand Name */}
-          {/* Left Aligned Items */}
+        {/* Logo and Brand Name */}
+        {/* Left Aligned Items */}
       </section>
     </nav>
-  )
+  );
 }
 
 export default Header;
